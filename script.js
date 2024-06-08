7b4682ec35a8cf9508e4756a656e6644701cdd2d
Diana Kosyanenko <kosyanenkodiana@yandex.ru>
2024-06-08 22:22:19 +0300

    new rep

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора, найти минимальное число в массиве, решение задание должно состоять из одной строки
+// const arr = [6, 5, 7, 9];
+// const minArray = Math.min(...arr);
+// console.log(minArray);
+// 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
+function createCounter () {
+    return {
+        increment(count){
+           return count+=1;
+        },
+        decrement(count){
+           return count -= 1;
+        }
+    }
+    
+}
+const result = createCounter();
+console.log(result.increment(2));
+console.log(result.decrement(2));
+// 3) Напишите рекурсивную функцию findElementByClass, которая принимает корневой элемент дерева DOM и название класса в качестве аргументов и возвращает первый найденный элемент с указанным классом в этом дереве.
+// Пример
+// const rootElement = document.getElementById('root');
+// const targetElement = findElementByClass(rootElement, 'my-class');
+// console.log(targetElement);
